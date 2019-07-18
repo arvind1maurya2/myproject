87d71fe762110b9d075c6125f0a19ecebcff6497
arvind1maurya2 <arvind1maurya2@gmail.com>
2019-07-18 12:07:05 +0530

    /commit_by_terminal

---
+++ helloworld/src/main/java/com/coveros/demo/helloworld/Test2.java
@@ -0,0 +1,10 @@
+package com.coveros.demo.helloworld;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("test2 class ");
+	}
+
+}
